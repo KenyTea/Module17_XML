2ef9fc83e658f691bf70aef6adf9bbffdd31d4d0
Васильева <@223_16>
2018-05-22 21:25:12 +0600

    Add project files.

---
+++ Module17_XML/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace Module17_XML
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //XmlDocument doc = new XmlDocument();
+
+            //XmlElement root = doc.CreateElement("User");
+            //XmlElement name = doc.CreateElement("Name");
+            //name.InnerText = "Alex";
+            //XmlAttribute atr = doc.CreateAttribute("IIin");
+            //atr.InnerText = "5675657656757";
+            ////1
+            //name.Attributes.Append(atr);
+            //// orr 2
+            ////name.SetAttribute("Iin", "5675657656757");
+            //root.AppendChild(name);
+            //doc.AppendChild(root);
+            //doc.Save("UserAlex.xml");
+
+            XmlDocument doc = new XmlDocument();
+            doc.Load("https://habrahabr.ru/rss/interesting/");
+
+            var rootElement = doc.DocumentElement;
+
+        }
+    }
+}
